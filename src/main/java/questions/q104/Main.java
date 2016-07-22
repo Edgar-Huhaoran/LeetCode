29f99faf473f96a0e21a09362c40460cedce1a35
Edgar-Huhaoran <edgarhuhaoran@gmail.com>
2016-07-22 17:29:16 +0800

    finish question 104

---
+++ src/main/java/questions/q104/Main.java
@@ -0,0 +1,16 @@
+package questions.q104;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/main/java/questions/q104/Solution.java
@@ -0,0 +1,32 @@
+package questions.q104;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftLen = 0;
+        int rightLen = 0;
+        if (root.left != null) {
+            leftLen = maxDepth(root.left);
+        }
+        if (root.right != null) {
+            rightLen = maxDepth(root.right);
+        }
+
+        return Math.max(leftLen, rightLen) + 1;
+    }
+
+}
