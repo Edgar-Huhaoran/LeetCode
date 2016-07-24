238813c27dfc164f425eac03eb880fc0e7686f09
Edgar-Huhaoran <edgarhuhaoran@gmail.com>
2016-07-24 17:08:33 +0800

    add detections package

---
+++ src/main/java/detections/Main.java
@@ -0,0 +1,12 @@
+package detections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] arr = " 1  2 ".split(" ");
+        for (String s : arr) {
+            System.out.println(s);
+        }
+    }
+
+}
